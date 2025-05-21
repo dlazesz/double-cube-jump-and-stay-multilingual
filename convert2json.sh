b7ce34ccc0e51c13dc69ac7e28cfa55b07e91435
Balázs Indig <dlazesz@users.noreply.github.com>
2025-05-21 16:00:31 +0200

    Make convert2json.sh more compatible

--- convert2json.sh
+++ convert2json.sh
@@ -7,24 +7,24 @@ then
 fi
 
 INPUTFILE=$1
-INPUTLANG=$(basename $INPUTFILE)
+INPUTLANG=$(basename "$INPUTFILE")
 # inputfile nevek kétbetűs nyelvazonosítók kellenek, hogy legyenek!
 
 ODIR=json
 
-OUTPUT=$(basename $INPUTFILE)
+OUTPUT=$(basename "$INPUTFILE")
 
 # 20 esetén már pár magyar ige is akad... :)
 FQTH=20
 
-cat $INPUTFILE | sed "s/.*stem@@//;s/ .*//" | sort | uniq -c | sort -nr | \
+# get verbs
+sed "s/.*stem@@//;s/ .*//" "$INPUTFILE" | sort | uniq -c | sort -nr | \
   awk -v fqth="$FQTH" '$1 >= fqth' | sed "s/^ *[0-9]* *//" \
-  > $ODIR/${INPUTLANG}.verbs
+  > "$ODIR/${INPUTLANG}.verbs"
 
-for VERB in `cat $ODIR/${INPUTLANG}.verbs`
-do
-  cat $INPUTFILE | egrep "stem@@$VERB( |$)" | sed "s/stem@@$VERB *//" | \
+while read -r VERB; do
+  grep -E "stem@@$VERB( |$)" "$INPUTFILE" | sed "s/stem@@$VERB *//" | \
     sed 's/^ *//;s/^\([0-9][0-9]*\)/{"fq": \1/;s/ @@[^ ][^ ]*//g;s/\([^ ][^ ]*\)@@\([^ ][^ ]*\)/, "\1": "\2"/g;s/$/}/;s/ *,/,/g;s/POSS//g' \
-    > $ODIR/${INPUTLANG}_${VERB}.test.json
-done
+    > "$ODIR/${INPUTLANG}_${VERB}.test.json"
+done < "$ODIR/${INPUTLANG}.verbs"
 
