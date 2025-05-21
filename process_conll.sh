04a1796c33f19fbf40e88a60d3771e2364ce96d5
Balázs Indig <dlazesz@users.noreply.github.com>
2025-05-21 15:18:43 +0200

    Improve compatibility of process_conll.sh

--- process_conll.sh
+++ process_conll.sh
@@ -7,15 +7,16 @@ then
 fi
 
 INPUTFILE=$1
-INPUTLANG=$(basename $INPUTFILE)
+INPUTLANG=$(basename "$INPUTFILE")
 # inputfile nevek kétbetűs nyelvazonosítók kellenek, hogy legyenek!
 
 ODIR=mazsdb
 
-BASE=$(basename $INPUTFILE)
+BASE=$(basename "$INPUTFILE")
 
 O1=${BASE}.verbose
 O2=${BASE}
 
-python3 ./process_conll.py --input-file $INPUTFILE --language $INPUTLANG > $ODIR/$O1
-cat $ODIR/$O1 | grep "stem@@" | sed "s/ _@@[^ ][^ ]*//g" | sort | uniq -c | sort -nr > $ODIR/$O2
+python3 ./process_conll.py --input-file "$INPUTFILE" --language "$INPUTLANG" > "$ODIR/$O1"
+grep -F "stem@@" "$ODIR/$O1" | sed "s/ _@@[^ ][^ ]*//g" | sort | uniq -c | sort -nr > "$ODIR/$O2"
+
