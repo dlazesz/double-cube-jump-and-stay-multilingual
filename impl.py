83724fca937c25c61ee627e8c5a64fb1141b35f3
Balázs Indig <dlazesz@users.noreply.github.com>
2026-05-02 20:47:17 +0200

    Simplify the processing function

--- impl.py
+++ impl.py
@@ -94,7 +94,7 @@ def build_dc_recursively(d, d_json, freq, vertices_freq, vertices_len, edges_bac
             build_dc_recursively(e, e_json, freq, vertices_freq, vertices_len, edges_backward, edges_forward, visited)
 
 
-def build_corpus_lattice(subject_slot) -> tuple[dict[str, int], dict[str, int], dict[str, set], dict[str, set]]:
+def build_corpus_lattice(subject_slot):
     # Corpus lattice
     cl_vertices_freq = Counter()  # Freq of vertices
     cl_vertices_len = {}  # Length of VCCs at vertices
@@ -145,8 +145,8 @@ def main():
             print(' Too long (>8), skip.')
         else:
             print(' Processing.')
-            process(i, cl_vertices_freq, cl_vertices_len, cl_edges_backward, cl_edges_forward,
-                    stay, jump1, jump2, jump3)
+            perform_jump_and_stay_steps(i, cl_vertices_freq, cl_vertices_len, cl_edges_backward, cl_edges_forward,
+                                        stay, jump1, jump2, jump3)
         print()
 
 
@@ -155,7 +155,7 @@ def print_full(i, cl_vertices_freq, cl_vertices_len, cl_edges_backward, cl_edges
 
     # Forward edges -- for "stay"
     # Sort: according to freq value, then vcc string-format key
-    for j in sorted(cl_edges_forward.get(i, set()), key=lambda x: (cl_vertices_freq[x], x)):
+    for j in sorted(cl_edges_forward.get(i, ()), key=lambda x: (cl_vertices_freq[x], x)):
         j_freq = cl_vertices_freq[j]
         ratio = freq / j_freq
         if ratio < stay:
@@ -169,7 +169,7 @@ def print_full(i, cl_vertices_freq, cl_vertices_len, cl_edges_backward, cl_edges
 
     # Backward edges -- for "jump"
     # Sort: according to freq value, then vcc string-format key
-    for j in sorted(cl_edges_backward.get(i, set()), key=lambda x: (cl_vertices_freq[x], x)):
+    for j in sorted(cl_edges_backward.get(i, ()), key=lambda x: (cl_vertices_freq[x], x)):
         j_freq = cl_vertices_freq[j]
         ratio = j_freq / freq
         if ratio < stay:
@@ -185,118 +185,94 @@ def print_full(i, cl_vertices_freq, cl_vertices_len, cl_edges_backward, cl_edges
     print(i, freq, cl_vertices_len[i], sep='\t')
 
 
-def process(act, cl_vertices_freq, cl_vertices_len, cl_edges_backward, cl_edges_forward, stay, jump1, jump2, jump3):
-    """How does it work
+def perform_jump_and_stay_steps(act, cl_vertices_freq, cl_vertices_len, cl_edges_backward, cl_edges_forward,
+                                stay, jump1, jump2, jump3):
+    """Perform the necessary amount of jumps and stays mixed:
         * Is there a stay? -> Perform the step defined by the smallest-ratio stay
-
         * If no stay, is there a jump? -> Perform the step defined by the largest-ratio jump
             iff there is a filler and there is a filler after the jump as well (jump1)
                 or there is no filler at all in the current vertex (jump2)
-
         * Repeat if a step was made
-
-       So it performs necessary amount of jumps and stays mixed
     """
 
     path = []  # = Log of jumps and stays
 
     while True:
-        stay_found = True
-        jump_found = True
+        freq_act = cl_vertices_freq[act]
 
         # Is there a stay?
-        # There are always one except at a sentence skeleton
         # Forward vertices
-        max_out: str | None = max(cl_edges_forward.get(act, set()), key=lambda x: (cl_vertices_freq[x], x),
-                                  default=None)
+        max_out: str | None = max(cl_edges_forward.get(act, ()), key=lambda x: (cl_vertices_freq[x], x), default=None)
 
-        freq_act = cl_vertices_freq[act]
-        freq_max_out = cl_vertices_freq.get(max_out, 0)
-        # Whether 'max_out' stays compared to 'act' (there must be an act..max_out edge!)
-        # Freq-ratio on act..max_out edge (there must be an act..max_out edge!)
-        if max_out is not None and freq_act / freq_max_out < stay:
-            print(' A stay found, we follow.')
-            path.append('v')
-            # Current vertex
-            print(max_out, freq_max_out, cl_vertices_len[max_out], sep='\t')
-            act = max_out
-
-        else:
+        # There are always one except at a sentence skeleton
+        if max_out is not None:
             # Freq-ratio on act..max_out edge (there must be an act..max_out edge!)
-            if max_out is not None:
-                r1 = freq_act / freq_max_out
-            else:
-                r1 = 0  # TODO here 0 do not satisfy the gt relation should be inf?
+            freq_max_out = cl_vertices_freq[max_out]
+            stay_ratio = freq_act / freq_max_out
 
-            print(f' No stay (ratio={r1:2.2f} > {stay}), we stop.')
-            stay_found = False
+            # Whether 'max_out' stays compared to 'act' (there must be an act..max_out edge!)
+            if stay_ratio < stay:
+                print(' A stay found, we follow.')
+                path.append('v')
+                # Current vertex
+                print(max_out, freq_max_out, cl_vertices_len[max_out], sep='\t')
+                act = max_out
+                continue
+        else:
+            stay_ratio = 0  # TODO here 0 do not satisfy the gt relation should be inf?
 
-            # If no stay, is there a(n appropriate) jump?
-            # There are always one except at root
-            # Backward vertices
-            max_inn: str | None = max(cl_edges_backward.get(act, set()), key=lambda x: (cl_vertices_freq[x], x),
-                                      default=None)
+        print(f' No stay (ratio={stay_ratio:2.2f} > {stay}), we stop.')
 
-            freq_max_inn = cl_vertices_freq[max_inn]
-            if max_inn is not None:  # This exists except at root :)
-                # Freq-ratio on max_inn..act edge (there must be an max_inn..act edge!)
-                r = freq_max_inn / freq_act
+        # If no stay, is there a(n appropriate) jump?
+        # Backward vertices
+        max_inn: str | None = max(cl_edges_backward.get(act, ()), key=lambda x: (cl_vertices_freq[x], x), default=None)
 
-                # Whether there is a filler in max_inn or act
-                # Need to look at values, whether there is a not-None
-                # There is a not-False (None is False)
-                has_filler_max_inn = any(json.loads(max_inn).values())
-                has_filler_act = any(json.loads(act).values())
-                # 3 different cases which covers all possibilities
-                # XOR: There is a filler and there is a filler after the jump as well
-                if has_filler_max_inn:
-                    jump = jump1
-                    info_msg = 'keeping a filler'
-                    jump_type = 't(k)'
-                # XOR: There is no filler at all in the current vertex
-                elif not has_filler_act:
-                    jump = jump2
-                    info_msg = 'no filler'
-                    jump_type = 't(n)'
-                # XOR: There is one filler and the jump omits it
-                elif has_filler_act and not has_filler_max_inn:
-                    jump = jump3
-                    info_msg = 'omitting last filler'
-                    jump_type = 't(o)'
-                else:
-                    print(' impossible outcome')
-                    exit(1)
-
-                # Check whether the jump is OK
-                # Is jump? Whether 'max_inn' jumps compared to 'act' (there must be a max_inn..act edge!)
-                # Freq-ratio on max_inn..act edge (there must be an max_inn..act edge!)
-                if freq_max_inn / freq_act > jump:
-                    print(f' An appropriate jump ({info_msg}, {jump}<) found, we follow.')
-                    path.append(jump_type)
-                    # Current vertex
-                    print(max_inn, freq_max_inn, cl_vertices_len[max_inn], sep='\t')
-                    act = max_inn
-                else:
-                    print(f' No appropriate jump ({info_msg}, {r:2.2f} < {jump}), we stop.')
-                    jump_found = False
-
-            else:
-                print(' No backward edge -- no jump, we stop.')
-                jump_found = False
-
-        # Quit the loop when no step was made
-        if not stay_found and not jump_found:
+        # There are always one except at root
+        if max_inn is None:
+            print(' No backward edge -- no jump, we stop.')
             break
 
-    # What to do when we are at an sentence skeleton
+        # Freq-ratio on max_inn..act edge (there must be an max_inn..act edge!)
+        freq_max_inn = cl_vertices_freq[max_inn]
+        jump_ratio = freq_max_inn / freq_act
+
+        # Whether there is a filler in max_inn or act
+        # Need to look at values, whether there is a not-None (not-False as None is False)
+        has_filler_max_inn = any(json.loads(max_inn).values())
+        has_filler_act = any(json.loads(act).values())
+
+        # 3 different cases which covers all possibilities
+        # XOR: There is a filler and there is a filler after the jump as well
+        if has_filler_max_inn:
+            jump, info, jump_type = jump1, 'keeping a filler', 't(k)'
+        # XOR: There is no filler at all in the current vertex
+        elif not has_filler_act:
+            jump, info, jump_type = jump2, 'no filler', 't(n)'
+        # XOR: There is one filler and the jump omits it
+        else:
+            jump, info, jump_type = jump3, 'omitting last filler', 't(o)'
+
+        # Is jump?
+        # Whether 'max_inn' jumps compared to 'act' (there must be a max_inn..act edge!)
+        if jump_ratio > jump:
+            print(f' An appropriate jump ({info}, {jump}<) found, we follow.')
+            path.append(jump_type)
+            # Current vertex
+            print(max_inn, freq_max_inn, cl_vertices_len[max_inn], sep='\t')
+            act = max_inn
+            continue
+
+        print(f' No appropriate jump ({info}, {jump_ratio:2.2f} < {jump}), we stop.')
+        break
+
+    # What to do when we are at an sentence skeleton?
     # Current implementation: no sentence skeleton can be a pVCC because there are pVCCS like 'shine sun'
     # Whether 'act' is a sentence skeleton
-    # XXX is this condition OK? "it has no forward edge" -- THINK ABOUT IT!
     if act not in cl_edges_forward:
         print(' Concrete sentence skeleton.')
     else:
-        # pathstr = '0' if not path else ''.join(path)
-        # print(act,freq_act, cl_vertices_len[act], f'[{pathstr}]', pVCC, sep='\t')
+        # pathstr = '0' if len(path) == 0 else ''.join(path)
+        # print(act, freq_act, cl_vertices_len[act], f'[{pathstr}]', pVCC, sep='\t')
         print(act, freq_act, cl_vertices_len[act], 'pVCC', sep='\t')
 
 
